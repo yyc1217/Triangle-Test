10e74b8d339c6ff5d2dfcca6723af68a543dc313
yyc1217 <yyc1217@gmail.com>
2013-07-30 14:16:22 +0800

    Update TriangleType.java

--- TriangleTest/src/main/TriangleType.java
+++ TriangleTest/src/main/TriangleType.java
@@ -1,9 +1,9 @@
 package main;
 
 public enum TriangleType {
-	TRIANGLE,		//三角形
+	TRIANGLE,	//三角形
 	EQUILATERAL,	//等邊三角形
-	ISOSCELES,		//等腰三角形
-	SCALENE,		//直角三角形
+	ISOSCELES,	//等腰三角形
+	SCALENE,	//直角三角形
 	NOT_A_TRIANGLE	//不是三角形
 }
